222f54133c569b9aea3d9d58478d589c92cf494d
david <mnace77@gmail.com>
2025-04-15 15:13:36 +0800

    完成題目

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div class="col-12 col-md-6 p-3">
       <div class="border rounded p-3">
         <h2>操作</h2>
+        <!-- <pre>{{ users }}</pre> -->
+        <!-- <pre>{{ users[(users.length-1)].id }}</pre> -->
         <form @submit.prevent>
           <BFormControls
               class="mb-3"
@@ -13,6 +15,7 @@
               class="mb-3"
               label="年齡"
               type="number"
+              value="formDate.age"
               v-model="formDate.age"/>
 
           <div class="d-flex gap-1">
@@ -66,7 +69,7 @@ interface User {
   age: number;
 }
 
-const baseUrl = '' // 由面試官提供
+const baseUrl = 'https://41133.wu.elitepro.ltd' // 由面試官提供
 const users = ref<User[]>([])
 const formDate = ref({
   // id readonly
@@ -75,21 +78,63 @@ const formDate = ref({
   age: 0,
 })
 
+
 const create = () => {
   // 需有確認步驟
+  if (formDate.value.name === '') {
+    alert('請輸入姓名及年齡');
+  } else if ( formDate.value.age === 0 ) {
+    alert('請輸入姓名及年齡');
+  } else{
+
+    const alertText = '確定新增' + formDate.value.name + '(' + formDate.value.age + ') 嗎?'
+    const result = confirm(alertText)
+    if (result) {
+      const userId = users.value[(users.value.length-1)].id + 1
+
+      users.value.push({
+        id: userId,
+        name: formDate.value.name,
+        age: formDate.value.age
+      })
+
+    } else {
+    }
+  } 
+
 }
 
 const edit = () => {
   // 需有確認步驟
+  const alertText = '確定修改' + formDate.value.name + '(' + formDate.value.age + ') 嗎?'
+  const result = confirm(alertText)
+  if (result) {
+    const userIndex = users.value.findIndex(item => item.id === formDate.value.id)
+    users.value[userIndex].name = formDate.value.name
+    users.value[userIndex].age = formDate.value.age
+    
+
+  } else {
+  }
 }
 
 
 const selectUser = (user: User) => {
   // 禁止使用 formDate.value = user
+  formDate.value.name = user.name
+  formDate.value.age = user.age
+  formDate.value.id = user.id
+  
 }
 
 const remove = (user: User) => {
   // 需有確認步驟
+  const alertText = '確定刪除嗎?'
+  const result = confirm(alertText)
+  if (result) {
+    users.value = users.value.filter(item => item.id !== user.id)
+  } else {
+  }
 }
 
 const getUsers = () => {
--- src/components/BFormControls.vue
+++ src/components/BFormControls.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <label for="" class="form-label"></label>
-    <input type="" class="form-control" id="" placeholder="">
+    <input 
+      :type="type"
+      class="form-control"
+      id=""
+      placeholder=""
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      >
   </div>
 </template>
 
 <script setup lang="ts">
-
+  defineProps({
+    label: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    value: String,
+    modelValue: [String, Number]
+  })
+  defineEmits(['update:modelValue'])
 </script>
 
 <style lang="scss" scoped>
